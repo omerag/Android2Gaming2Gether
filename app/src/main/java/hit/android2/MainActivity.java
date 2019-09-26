d2e50de2fd67ffa310889cea8795aa64c59b800e
Balazs <Hungary3176>
2019-09-26 14:16:28 +0300

    navBar

--- app/src/main/java/hit/android2/MainActivity.java
+++ app/src/main/java/hit/android2/MainActivity.java
@@ -1,8 +1,12 @@
 package hit.android2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_bar);
+        bottomNavBarListener bottomNavBarListener = new bottomNavBarListener();
+        bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavBarListener);
     }
+
+    class bottomNavBarListener implements BottomNavigationView.OnNavigationItemSelectedListener
+    {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+            switch (menuItem.getItemId())
+            {
+                case R.id.nav_profile:
+                    break;
+                case R.id.nav_messages:
+                    break;
+            }
+
+            return true;
+        }
+    }
+
 }
