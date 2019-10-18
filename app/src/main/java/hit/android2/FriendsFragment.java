8f003dcc96265de5b67df33b3af551b4d9c3950c
Balazs <Hungary3176>
2019-10-18 15:24:11 +0300

    friend profile

--- app/src/main/java/hit/android2/FriendsFragment.java
+++ app/src/main/java/hit/android2/FriendsFragment.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.PopupMenu;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
