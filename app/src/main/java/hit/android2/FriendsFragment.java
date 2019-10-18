9b3ec28c9b6be4c9b70d95edee4c4231abbf99a7
Balazs <Hungary3176>
2019-10-18 13:22:48 +0300

    chat improvment

--- app/src/main/java/hit/android2/FriendsFragment.java
+++ app/src/main/java/hit/android2/FriendsFragment.java
@@ -3,7 +3,9 @@ package hit.android2;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
@@ -11,6 +13,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -67,11 +70,33 @@ public class FriendsFragment extends Fragment {
         userAdapter.notifyDataSetChanged();
 
         userAdapter.setListener(new UserAdapter.AdapterListener() {
+
             @Override
-            public void onClick(View view, int position) {
-                Intent intent = new Intent(getActivity(), MessagingActivity.class);
-                intent.putExtra("user_id", friendsList.get(position).getKey());
-                getActivity().startActivity(intent);
+            public void onClick(final View view, int position) {
+
+                final int viewPosition = position;
+                PopupMenu popupMenu = new PopupMenu(getContext(), view);
+                popupMenu.getMenuInflater().inflate(R.menu.friends_popup_menu, popupMenu.getMenu());
+
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+
+                        switch (item.getItemId())
+                        {
+                            case R.id.menu_item_view_profile:
+                                break;
+
+                            case R.id.menu_item_send_message:
+                                Intent intent = new Intent(getActivity(), MessagingActivity.class);
+                                intent.putExtra("user_id", friendsList.get(viewPosition).getKey());
+                                getActivity().startActivity(intent);
+                                break;
+                        }
+                        return true;
+                    }
+                });
+                popupMenu.show();
             }
         });
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
