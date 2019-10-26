6ad68c7fecde0cbd3628ffddeba1689cf6c06f7a
Balazs <bberes92@gmail.com>
2019-10-26 18:35:50 +0300

    sign up fragment

--- app/src/main/java/hit/android2/LoginFragment.java
+++ app/src/main/java/hit/android2/LoginFragment.java
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.textfield.TextInputEditText;
 
 import hit.android2.Database.Managers.FirebaseManager;
 
@@ -25,8 +26,8 @@ public class LoginFragment extends Fragment {
     private ImageButton back_btn;
 
 
-    private EditText emailInput;
-    private EditText passwordInput;
+    private TextInputEditText emailInput;
+    private TextInputEditText passwordInput;
 
     private FirebaseManager firebaseManager;
 
