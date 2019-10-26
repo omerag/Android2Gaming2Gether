5e45b5f5f81f2ac6e848c30854207f0f0b70c1fd
Balazs <bberes92@gmail.com>
2019-10-26 13:57:25 +0300

    friend search fragment

--- app/src/main/java/hit/android2/SearchFriendFragment.java
+++ app/src/main/java/hit/android2/SearchFriendFragment.java
@@ -1,10 +1,7 @@
 package hit.android2;
 
-import android.app.Dialog;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
@@ -14,225 +11,82 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.widget.PopupMenu;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.viewpager.widget.ViewPager;
 
-import com.bumptech.glide.Glide;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import de.hdodenhof.circleimageview.CircleImageView;
 import hit.android2.Adapters.FlagAdapter;
 import hit.android2.Adapters.GameAdapter;
+import hit.android2.Adapters.UserAdapter;
 import hit.android2.Database.Managers.DatabaseManager;
 import hit.android2.Database.Managers.FirebaseManager;
 import hit.android2.Database.Model.GameData;
 import hit.android2.Database.Model.UserData;
-import hit.android2.Adapters.UserAdapter;
 
-public class FriendsFragment extends Fragment {
+public class SearchFriendFragment extends Fragment {
 
     private UserAdapter userAdapter;
-    private List<UserData> friendsList = new ArrayList<>();
-    private RecyclerView recyclerView;
-    private FloatingActionButton floatingActionButton;
-
+    private List<UserData> friendsList;
     private FriendsFragmentLiveData liveData;
-    private BottomNavigationView bottomNavigationView;
-    private ViewPager pager;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.friends_fragment,container,false);
-
-        View view = getActivity().findViewById(R.id.bottom_navigation_bar);
-        bottomNavigationView = (BottomNavigationView) view;
-
-        View view1 = getActivity().findViewById(R.id.fragment_container);
-        pager = (ViewPager) view1;
+        View rootView = inflater.inflate(R.layout.search_friends_dialog_layout, container, false);
 
 
         return rootView;
     }
 
+    public SearchFriendFragment(UserAdapter userAdapter, List<UserData> friendsList, FriendsFragmentLiveData liveData) {
+        this.userAdapter = userAdapter;
+        this.friendsList = friendsList;
+        this.liveData = liveData;
+    }
+
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-        liveData = ViewModelProviders.of(this).get(FriendsFragmentLiveData.class);
-
-
-        recyclerView =getView().findViewById(R.id.friends_fragment_recycler_users);
-        floatingActionButton = getView().findViewById(R.id.friends_fragment_floating_action_btn);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
-        recyclerView.setLayoutManager(linearLayoutManager);
-
-        userAdapter = new UserAdapter(getActivity(), friendsList); //friendsList is empty, needs to be loaded from server
-
-        recyclerView.setAdapter(userAdapter);
-        userAdapter.notifyDataSetChanged();
-
-        userAdapter.setListener(new UserAdapter.AdapterListener() {
-
-            @Override
-            public void onClick(final View view, int position) {
-
-                final int viewPosition = position;
-                PopupMenu popupMenu = new PopupMenu(getContext(), view);
-                popupMenu.getMenuInflater().inflate(R.menu.friends_popup_menu, popupMenu.getMenu());
-
-                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                    @Override
-                    public boolean onMenuItemClick(MenuItem item) {
-
-                        switch (item.getItemId())
-                        {
-                            case R.id.menu_item_view_profile:
-                                ShowFriendProfile(friendsList.get(viewPosition).getKey());
-                                break;
-
-                            case R.id.menu_item_send_message:
-                                Intent intent = new Intent(getActivity(), MessagingActivity.class);
-                                intent.putExtra("user_id", friendsList.get(viewPosition).getKey());
-                                getActivity().startActivity(intent);
-                                break;
-                        }
-                        return true;
-                    }
-                });
-                popupMenu.show();
-            }
-        });
-        floatingActionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //showSearchFriendDialog();
-                getActivity().getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.dialog_fragments_container, new SearchFriendFragment(userAdapter, friendsList, liveData))
-                        .addToBackStack("searchFriendFragment").commit();
-
-                bottomNavigationView.setVisibility(View.INVISIBLE);
-                pager.setVisibility(View.INVISIBLE);
-            }
-        });
-
-        if(FirebaseManager.isLoged()){
-            loadFriends();
-        }
-    }
-
-    private void ShowFriendProfile(String friend_id)
-    {
-        final Dialog dialog = new Dialog(getActivity());
-        final List<UserData> userData = new ArrayList<>();
-
-        dialog.setContentView(R.layout.user_profile_dialog);
-
-        dialog.setTitle("Friend Profile");
-
-        final CircleImageView profile_image = dialog.findViewById(R.id.user_profile_img);
-        final TextView user_name = dialog.findViewById(R.id.profile_fragment_user_name);
-        final TextView user_about_me = dialog.findViewById(R.id.about_me_tv);
-        List<GameData> games = new ArrayList<>();
-        final GameAdapter gameAdapter = new GameAdapter(getActivity(),games);
-        final RecyclerView recyclerView = dialog.findViewById(R.id.profile_fragment_recycler_games);
-
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(),RecyclerView.HORIZONTAL,false);
-        recyclerView.setLayoutManager(linearLayoutManager);
-
-
-
-        DatabaseManager.getUserFromDatabase(friend_id, userData, new DatabaseManager.Listener() {
-            @Override
-            public void onSuccess() {
-                user_name.setText(userData.get(0).getName());
-                user_about_me.setText(userData.get(0).getAboutMe());
-                Glide.with(getContext()).load(userData.get(0).getImageUrl()).into(profile_image);
-            }
-        });
-
-        DatabaseManager.getUserGames(friend_id, games, gameAdapter, new DatabaseManager.Listener() {
-            @Override
-            public void onSuccess() {
-                recyclerView.setAdapter(gameAdapter);
-                gameAdapter.notifyDataSetChanged();
-            }
-        });
-
-
-        dialog.show();
-        dialog.getCurrentFocus();
-    }
-
-    private void loadFriends(){
-        if(liveData.friendsList == null ){
-            DatabaseManager.getUserFriends(FirebaseAuth.getInstance().getCurrentUser().getUid(), friendsList, new DatabaseManager.Listener() {
-                @Override
-                public void onSuccess() {
-                    liveData.setFriendsList(friendsList);
-                    userAdapter.notifyDataSetChanged();
-                }
-            });
-        }
-        else {
-            friendsList = liveData.getFriendsList();
-            userAdapter.setUserDataList(friendsList);
-            userAdapter.notifyDataSetChanged();
-        }
-
-
-    }
-
-    /*private void showSearchFriendDialog(){
-
-        final Dialog dialog = new Dialog(getActivity());
-        dialog.setContentView(R.layout.search_friends_dialog_layout);
-        dialog.setTitle("Search Friends Dialog");
-
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         String generResult = "all";
         final String[] languegeResult = {"english"};
         String rankTypeResult = "totalRank";
         final int[] age = {0};
 
-        ImageButton searchBtn = dialog.findViewById(R.id.search_button);
+        ImageButton searchBtn = getView().findViewById(R.id.search_button);
         searchBtn.setTag(R.id.search_button);
 
 
         final GetStringFromImageBtn stringFromImageBtn =  new GetStringFromImageBtn();
-        ImageButton maleGenderIbtn = dialog.findViewById(R.id.search_friends_dialog_gender_male);
+        ImageButton maleGenderIbtn = getView().findViewById(R.id.search_friends_dialog_gender_male);
         maleGenderIbtn.setTag(R.id.search_friends_dialog_gender_male);
         maleGenderIbtn.setOnClickListener(stringFromImageBtn);
-        ImageButton femaleGenderIbtn = dialog.findViewById(R.id.search_friends_dialog_gender_female);
+        ImageButton femaleGenderIbtn = getView().findViewById(R.id.search_friends_dialog_gender_female);
         femaleGenderIbtn.setTag(R.id.search_friends_dialog_gender_female);
         femaleGenderIbtn.setOnClickListener(stringFromImageBtn);
-        ImageButton allGenderIbtn = dialog.findViewById(R.id.search_friends_dialog_gender_all);
+        ImageButton allGenderIbtn = getView().findViewById(R.id.search_friends_dialog_gender_all);
         allGenderIbtn.setTag(R.id.search_friends_dialog_gender_all);
         allGenderIbtn.setOnClickListener(stringFromImageBtn);
 
-        ImageButton totalRankIbtn = dialog.findViewById(R.id.search_friends_dialog_overall_level);
+        ImageButton totalRankIbtn = getView().findViewById(R.id.search_friends_dialog_overall_level);
         totalRankIbtn.setTag(R.id.search_friends_dialog_overall_level);
         totalRankIbtn.setOnClickListener(stringFromImageBtn);
-        ImageButton teammateRankIbtn = dialog.findViewById(R.id.search_friends_dialog_teammate_level);
+        ImageButton teammateRankIbtn = getView().findViewById(R.id.search_friends_dialog_teammate_level);
         teammateRankIbtn.setTag(R.id.search_friends_dialog_teammate_level);
         teammateRankIbtn.setOnClickListener(stringFromImageBtn);
-        ImageButton spotmanRankIbtn = dialog.findViewById(R.id.search_friends_dialog_sportsmanship_level);
+        ImageButton spotmanRankIbtn = getView().findViewById(R.id.search_friends_dialog_sportsmanship_level);
         spotmanRankIbtn.setTag(R.id.search_friends_dialog_sportsmanship_level);
         spotmanRankIbtn.setOnClickListener(stringFromImageBtn);
-        ImageButton leaderRankIbtn = dialog.findViewById(R.id.search_friends_dialog_leader_level);
+        ImageButton leaderRankIbtn = getView().findViewById(R.id.search_friends_dialog_leader_level);
         leaderRankIbtn.setTag(R.id.search_friends_dialog_leader_level);
         leaderRankIbtn.setOnClickListener(stringFromImageBtn);
 
-        final TextView ageTv = dialog.findViewById(R.id.search_friends_dialog_age_tv);
-        SeekBar ageSeekBat = dialog.findViewById(R.id.search_friends_seekbar_age);
+        final TextView ageTv = getView().findViewById(R.id.search_friends_dialog_age_tv);
+        SeekBar ageSeekBat = getView().findViewById(R.id.search_friends_seekbar_age);
         ageSeekBat.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
@@ -258,7 +112,7 @@ public class FriendsFragment extends Fragment {
 
 
         ///flags recycler
-        final RecyclerView flagsRecycler = dialog.findViewById(R.id.search_friends_dialog_flags_recycler_view);
+        final RecyclerView flagsRecycler = getView().findViewById(R.id.search_friends_dialog_flags_recycler_view);
         flagsRecycler.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
         FlagAdapter flagAdapter = new FlagAdapter();
         flagsRecycler.setAdapter(flagAdapter);
@@ -275,7 +129,7 @@ public class FriendsFragment extends Fragment {
         ///
 
         ///games recycler
-        final RecyclerView gamesRecycler = dialog.findViewById(R.id.search_friends_dialog_games_recycler_view);
+        final RecyclerView gamesRecycler = getView().findViewById(R.id.search_friends_dialog_games_recycler_view);
         gamesRecycler.setLayoutManager(new LinearLayoutManager(getActivity(),LinearLayoutManager.HORIZONTAL,false));
         final List<UserData> users = new ArrayList<>();
         final List<GameData> games = new ArrayList<>();
@@ -325,7 +179,6 @@ public class FriendsFragment extends Fragment {
                                     public void onSuccess() {
                                         liveData.setFriendsList(friendsList);
                                         userAdapter.notifyDataSetChanged();
-                                        dialog.dismiss();
                                     }
                                 });
                             }
@@ -337,9 +190,8 @@ public class FriendsFragment extends Fragment {
 
 
         DatabaseManager.getUserGames(FirebaseAuth.getInstance().getCurrentUser().getUid(),games,gameAdapter);
-        dialog.show();
-        dialog.getCurrentFocus();
 
+        super.onViewCreated(view, savedInstanceState);
     }
 
     class GetStringFromImageBtn implements View.OnClickListener{
@@ -392,5 +244,5 @@ public class FriendsFragment extends Fragment {
 
 
         }
-    }*/
+    }
 }
